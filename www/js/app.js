4e29f1d501e898f0c1d2d355a1578e0615e84eb8
Matt Spaulding <mrmattspaulding@gmail.com>
2014-09-12 01:09:44 -0400

    no message

--- www/js/app.js
+++ www/js/app.js
@@ -2,15 +2,15 @@
 	var app = angular.module('myApp', ['onsen.directives']);
 
 	app.controller('StreamCtrl', function($scope, $http) { debugger;
-		$scope.myData = {};
-		$scope.myData.doClick = function(item, event) {
+		$scope.response = {};
+		$scope.response.get = function(item, event) {
 
 			var responsePromise = $http.get("http://blrbrdev.azurewebsites.net/Blrb/StreamJson");
 
 			responsePromise.success(function(data, status, headers, config) { debugger;
 				alert("AJAX success!");
-				alert("data: "+data.NumberOfBlrbs);
-				$scope.BlrbStreamResponse = data;
+				alert("data: " + data.NumberOfBlrbs);
+				$scope.response = data;
 			});
 			responsePromise.error(function(data, status, headers, config) { debugger;
 				// alert("AJAX failed! "+status);
@@ -19,36 +19,7 @@
 			});
 
 		};
-	$scope.myData.doUser = function(item, event) {
 
-			var responsePromise = $http.get("http://blrbrdev.azurewebsites.net/Blrb/TestUserJson");
-
-			responsePromise.success(function(data, status, headers, config) { debugger;
-				alert("AJAX success!");
-				$scope.myData.fromServer = data.title;
-			});
-			responsePromise.error(function(data, status, headers, config) { debugger;
-				alert("AJAX failed! "+status);
-				debugger;
-				gotoRoute("Account/Login");
-			});
-
-		};
-	$scope.myData.doNoUser = function(item, event) {
-
-			var responsePromise = $http.get("http://blrbrdev.azurewebsites.net/Blrb/TestNoUserJson");
-
-			responsePromise.success(function(data, status, headers, config) { debugger;
-				alert("AJAX success!");
-				$scope.myData.fromServer = data.title;
-			});
-			responsePromise.error(function(data, status, headers, config) { debugger;
-				alert("AJAX failed! "+status);
-				debugger;
-				gotoRoute("Account/Login");
-			});
-
-		};
 	});
 
 	app.controller('CreateCtrl', function($scope) {
