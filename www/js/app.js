344095dedcfec1a43d06462aeeacf9877d769395
Matt Spaulding <mrmattspaulding@gmail.com>
2014-09-11 22:42:35 -0400

    Adding login.

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,24 @@
-(function(){
-	'use strict';
-	angular.module('myApp', ['onsen.directives']);
+(function() {'use strict';
+	var app = angular.module('myApp', ['onsen.directives']);
+
+	app.controller('StreamCtrl', function($scope, $http) { debugger;
+		$scope.myData = {};
+		$scope.myData.doClick = function(item, event) {
+
+			var responsePromise = $http.get("http://blrbrdev.azurewebsites.net/Blrb/StreamJson");
+
+			responsePromise.success(function(data, status, headers, config) { debugger;
+				$scope.myData.fromServer = data.title;
+			});
+			responsePromise.error(function(data, status, headers, config) { debugger;
+				alert("AJAX failed!");
+				debugger;
+				route("Account/Login");
+			});
+
+		};
+	});
+
+	app.controller('CreateCtrl', function($scope) {
+	});
 })();
