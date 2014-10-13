c30687d11a2cb18c3a31bbe70e5c5fa3391a7c1a
Matt Spaulding <mrmattspaulding@gmail.com>
2014-10-12 23:45:10 -0400

    no message

--- www/js/page-player.js
+++ www/js/page-player.js
@@ -337,7 +337,7 @@ function PagePlayer() {
 			} else {
 				device = "android";
 			}
-			$.get("http://blrbrdev.azurewebsites.net/Blrb/LogListen/" + blrbId + "?device=" + device, function(result) {
+			$.get("http://blrbr.co/Blrb/LogListen/" + blrbId + "?device=" + device, function(result) {
 
 			});
 			$('#liveItem').css("display", "none");
